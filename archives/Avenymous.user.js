afbc0508bcdcd17322d9284cbe49f4373ead8d0e
TabbyGarf <87911854+TabbyGarf@users.noreply.github.com>
2024-03-21 00:11:54 +0100

    Update Avenymous.user.js

--- archives/Avenymous.user.js
+++ archives/Avenymous.user.js
@@ -3,7 +3,7 @@
 // @namespace    http://tampermonkey.net/
 // @version      2024-01-10
 // @description  try to take over the world!
-// @author       You
+// @author       Encephallus
 // @match        https://avenoel.org/*
 // @icon         https://www.google.com/s2/favicons?sz=64&domain=avenoel.org
 // @grant        none
